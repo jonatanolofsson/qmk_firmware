96251210d203421d1f013888c35891d3ebe10831
Jonatan Olofsson <jonatan.olofsson@gmail.com>
2024-01-21 19:44:37 +0100

    Updated keymap

--- keyboards/voyager/keymaps/mine/keymap.c
+++ keyboards/voyager/keymaps/mine/keymap.c
@@ -24,6 +24,7 @@ enum tap_dance_codes {
   DANCE_AT,
   DANCE_DLR,
   DANCE_LT,
+  DANCE_PLUS,
   NUM_DANCES,
 };
 
@@ -43,7 +44,7 @@ enum layers {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [LAYER_0] = LAYOUT_voyager(
     KC_ESCAPE,      KC_1,           KC_2,           KC_3,           KC_4,           KC_5,                                           KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           SE_MINS,
-    KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,                                           KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           SE_PLUS,
+    KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,                                           KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           SE_EQL,
     TD(DANCE_LSFT), KC_A,           KC_S,           KC_D,           KC_F,           KC_G,                                           KC_H,           KC_J,           KC_K,           KC_L,           SE_SCLN,        SE_QUOT,
     TD(DANCE_PINKY),LT(LAYER_FN,KC_Z), KC_X,        KC_C,           KC_V,           KC_B,                                           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         SE_SLSH,        SE_TILD,
                                                     TD(DANCE_ENTER),TD(DANCE_LTHUMB),                               KC_BACKSPACE,   LT(LAYER_SYS,KC_SPACE)
@@ -57,15 +58,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
   [LAYER_SHIFT] = LAYOUT_voyager(
     KC_TRANSPARENT, KC_EXLM,        TD(DANCE_AT),   KC_HASH,        TD(DANCE_DLR),  KC_PERC,                                        SE_CIRC,        SE_AMPR,        SE_ASTR,        SE_LPRN,        SE_RPRN,        KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, SE_EQL,
+    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, TD(DANCE_PLUS),
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, SE_COLN,        SE_DQUO,
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, TD(DANCE_LT),   SE_RABK,        SE_QUES,        SE_GRV,
+    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, TD(DANCE_LT),   SE_RABK,        SE_QUES,        SE_BSLS,
                                                     KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT
   ),
   [LAYER_SSHIFT] = LAYOUT_voyager(
     KC_TRANSPARENT, KC_EXLM,        TD(DANCE_AT),   KC_HASH,        TD(DANCE_DLR),  KC_PERC,                                        SE_CIRC,        SE_AMPR,        SE_ASTR,        SE_LPRN,        SE_RPRN,        KC_TRANSPARENT,
     SE_TILD,        KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, SE_QUES,        SE_GRV,
+    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, SE_QUES,        SE_DQUO,
                                                     KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT
   ),
   [LAYER_CTL] = LAYOUT_voyager(
@@ -85,7 +87,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [LAYER_ALT] = LAYOUT_voyager(
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, LCTL(LALT(KC_DELETE)),
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-    KC_LSFT,        KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+    KC_LSFT,        KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_LEFT,        KC_DOWN,        KC_UP,          KC_RIGHT,       KC_TRANSPARENT, KC_TRANSPARENT,
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
                                                     KC_TRANSPARENT, KC_ENTER,                                       KC_TRANSPARENT, KC_ENTER
   ),
@@ -93,12 +95,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESCAPE,      KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,                                          KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_F11,
     SE_GRV,         KC_EXLM,        SE_AT,          SE_EURO,        SE_EURO,        KC_PERC,                                        KC_7,           KC_8,           KC_9,           SE_MINS,        SE_SLSH,        KC_F12,
     KC_TRANSPARENT, KC_CIRC,        SE_AMPR,        SE_ASTR,        SE_LPRN,        SE_RPRN,                                        KC_4,           KC_5,           KC_6,           SE_PLUS,        SE_ASTR,        KC_BACKSPACE,
-    KC_TRANSPARENT, SE_TILD,        SE_LBRC,        SE_RBRC,        SE_LCBR,        SE_RCBR,                                        KC_1,           KC_2,           KC_3,           KC_DOT,         SE_BSLS,         KC_ENTER,
+    KC_TRANSPARENT, SE_TILD,        SE_LBRC,        SE_RBRC,        SE_LCBR,        SE_RCBR,                                        KC_1,           KC_2,           KC_3,           KC_DOT,         SE_BSLS,        SE_GRV,
                                                                     KC_LALT,        KC_LCTL,                        KC_DELETE,      KC_0
   ),
   [LAYER_SYS] = LAYOUT_voyager(
     RGB_TOG,        TOGGLE_LAYER_COLOR,         RGB_MOD,            RGB_SLD,            RGB_VAD,            RGB_VAI,                            KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_AUDIO_MUTE,  KC_AUDIO_VOL_UP,
-    KC_TRANSPARENT, KC_TRANSPARENT,             KC_TRANSPARENT,     KC_TRANSPARENT,     KC_TRANSPARENT,     KC_TRANSPARENT,                     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_AUDIO_VOL_DOWN,
+    KC_TRANSPARENT, KC_TRANSPARENT,             KC_TRANSPARENT,     KC_TRANSPARENT,     KC_TRANSPARENT,     KC_TRANSPARENT,                     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_F20,         KC_AUDIO_VOL_DOWN,
     KC_TRANSPARENT, KC_MEDIA_PREV_TRACK,        KC_MEDIA_NEXT_TRACK,KC_MEDIA_STOP,      KC_MEDIA_PLAY_PAUSE,KC_TRANSPARENT,                     KC_LEFT,        KC_DOWN,        KC_UP,          KC_RIGHT,       KC_PGUP,        KC_BRIGHTNESS_UP,
     KC_LCTL,        KC_TRANSPARENT,             KC_TRANSPARENT,     HSV_0_255_255,      HSV_74_255_255,     HSV_169_255_255,                    KC_HOME,        KC_END,         LCTL(LSFT(KC_TAB)),LCTL(KC_TAB),KC_PGDN,        KC_BRIGHTNESS_DOWN,
                                                                                         KC_TRANSPARENT,     KC_LCTL,                            KC_TRANSPARENT, KC_TRANSPARENT
@@ -117,9 +119,9 @@ const uint8_t PROGMEM ledmap[][RGB_MATRIX_LED_COUNT][3] = {
     [LAYER_ALT] = {
         {0,0,0}, {0,0,255}, {0,0,255}, {0,0,255}, {0,0,255}, {0,0,255},
         {169,255,255}, {0,245,245}, {131,255,255}, {0,0,0}, {0,0,0}, {0,0,0},
-        {169,255,255}, {74,255,255}, {0,0,0}, {169,255,255}, {41,255,255}, {131,255,255},
-        {0,0,0}, {0,0,0}, {0,0,0}, {152,255,255}, {131,255,255}, {0,0,0},
-        {0,0,0}, {0,0,0},
+        {169,255,255}, {74,255,255}, {0,0,0}, {169,255,255}, {41,255,255}, {0,245,245},
+        {0,0,0}, {0,0,0}, {0,0,0}, {152,255,255}, {131,255,255}, {131,255,255},
+        {0,0,0}, {255,0,0},
 
         {0,0,255}, {0,0,255}, {0,0,255}, {0,0,255}, {0,0,255}, {0,0,0},
         {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0},
@@ -135,7 +137,7 @@ const uint8_t PROGMEM ledmap[][RGB_MATRIX_LED_COUNT][3] = {
         {0,0,0}, {169,255,84},
 
         {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,245,245}, {74,255,255},
-        {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {131,255,255},
+        {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,245,245}, {131,255,255},
         {169,255,255}, {169,255,255}, {169,255,255}, {169,255,255}, {218,218,204}, {0,0,255},
         {74,255,255}, {41,255,255}, {131,255,255}, {131,255,255}, {218,218,204}, {169,255,84},
         {0,0,0}, {0,0,0} },
@@ -239,7 +241,8 @@ void dance_lsft_finished(tap_dance_state_t *state, void *user_data) {
     switch (dance_state[DANCE_LSFT].step) {
         case SINGLE_HOLD:
             register_code16(KC_LSFT);
-            layer_on(IS_LAYER_ON(LAYER_SE) ? LAYER_SSHIFT : LAYER_SHIFT);
+            layer_on(LAYER_SHIFT);
+            if (IS_LAYER_ON(LAYER_SE)) layer_on(LAYER_SSHIFT);
             break;
         case DOUBLE_TAP: layer_invert(LAYER_SE); break;
     }
@@ -364,7 +367,8 @@ void dance_pinky_reset(tap_dance_state_t *state, void *user_data) {
 
 void dance_at(tap_dance_state_t *state, void *user_data) { unregister_code16(KC_LSFT); tap_code16(SE_AT); register_code16(KC_LSFT);}
 void dance_dlr(tap_dance_state_t *state, void *user_data) { unregister_code16(KC_LSFT); tap_code16(SE_DLR); register_code16(KC_LSFT);}
-void dance_lt(tap_dance_state_t *state, void *user_data) { unregister_code16(KC_LSFT);  tap_code16(SE_LABK); register_code16(KC_LSFT);}
+void dance_lt(tap_dance_state_t *state, void *user_data) { unregister_code16(KC_LSFT); tap_code16(SE_LABK); register_code16(KC_LSFT);}
+void dance_plus(tap_dance_state_t *state, void *user_data) { unregister_code16(KC_LSFT); tap_code16(SE_PLUS); register_code16(KC_LSFT);}
 
 tap_dance_action_t tap_dance_actions[] = {
         [DANCE_LSFT] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_lsft_finished, dance_lsft_reset),
@@ -374,6 +378,7 @@ tap_dance_action_t tap_dance_actions[] = {
         [DANCE_AT] = ACTION_TAP_DANCE_FN_ADVANCED(dance_at, NULL, NULL),
         [DANCE_DLR] = ACTION_TAP_DANCE_FN_ADVANCED(dance_dlr, NULL, NULL),
         [DANCE_LT] = ACTION_TAP_DANCE_FN_ADVANCED(dance_lt, NULL, NULL),
+        [DANCE_PLUS] = ACTION_TAP_DANCE_FN_ADVANCED(dance_plus, NULL, NULL),
 };
 
 uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
